9e87e74482ed6f8926419fe67ab25e26ec6d0de0
miklisanton <miklis2003@mail.ru>
2024-06-03 17:24:30 +0300

    feat: binance futures orderbook retrievement

--- go.mod
+++ go.mod
@@ -8,8 +8,18 @@ require (
 )
 
 require (
-	github.com/mattn/go-runewidth v0.0.2 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell v1.4.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.4 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
+	github.com/rivo/tview v0.0.0-20240524063012-037df494fb76 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/emirpasic/gods/maps/treemap"
+	"github.com/emirpasic/gods/utils"
 	"github.com/gorilla/websocket"
 	"io"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 type BinanceDepthResponse struct {
@@ -27,14 +31,68 @@ type BinanceDepthSnap struct {
 	Bids         [][]string `json:"bids"`
 }
 
-type Orderbook struct {
-	Asks map[float64]float64
-	Bids map[float64]float64
+type OrderBook struct {
+	mu         sync.Mutex
+	Asks       *treemap.Map
+	Bids       *treemap.Map
+	lowestAsk  float64
+	highestBid float64
+}
+
+func NewOrderBook() *OrderBook {
+	return &OrderBook{
+		Bids: treemap.NewWith(utils.Float64Comparator),
+		Asks: treemap.NewWith(utils.Float64Comparator),
+	}
+}
+
+func (ob *OrderBook) AddBid(price float64, quantity float64) {
+	ob.Bids.Put(price, quantity)
+	if price > ob.highestBid {
+		ob.highestBid = price
+	}
+}
+
+func (ob *OrderBook) AddAsk(price float64, quantity float64) {
+	ob.Asks.Put(price, quantity)
+	if price < ob.lowestAsk || ob.lowestAsk == 0 {
+		ob.lowestAsk = price
+	}
+}
+
+func (ob *OrderBook) RemoveBid(price float64) {
+	ob.Bids.Remove(price)
+	if price == ob.highestBid {
+		ob.highestBid, _, _ = ob.GetBestBid()
+	}
+}
+
+func (ob *OrderBook) RemoveAsk(price float64) {
+	ob.Asks.Remove(price)
+	if price == ob.lowestAsk {
+		ob.lowestAsk, _, _ = ob.GetBestAsk()
+	}
+}
+
+func (ob *OrderBook) GetBestBid() (float64, float64, bool) {
+	if ob.Bids.Empty() {
+		return 0, 0, false
+	}
+	bestBidKey, bestBidValue := ob.Bids.Max()
+	return bestBidKey.(float64), bestBidValue.(float64), true
+}
+
+func (ob *OrderBook) GetBestAsk() (float64, float64, bool) {
+	if ob.Asks.Empty() {
+		return 0, 0, false
+	}
+	bestAskKey, bestAskValue := ob.Asks.Min()
+	return bestAskKey.(float64), bestAskValue.(float64), true
 }
 
 // Gets depth snapshot from binance, endpoint must be provided
 // updates bids and asks and returns lastUpdatedId
-func (o *Orderbook) fetch(endpoint string) (int, error) {
+func (ob *OrderBook) fetch(endpoint string) (int, error) {
 	log.Println("Fetching orderbook ", endpoint)
 	resp, err := http.Get(endpoint)
 	if err != nil {
@@ -56,9 +114,12 @@ func (o *Orderbook) fetch(endpoint string) (int, error) {
 	if err := json.Unmarshal(body, &snap); err != nil {
 		return -1, fmt.Errorf("Error unmarshalling json: %s\n", err)
 	}
-	log.Println(body)
+
+	ob.mu.Lock()
+	defer ob.mu.Unlock()
 
 	// Copying bids
+	ob.Bids.Clear()
 	for _, bid := range snap.Bids {
 		var (
 			price  float64
@@ -73,10 +134,11 @@ func (o *Orderbook) fetch(endpoint string) (int, error) {
 			log.Println("Error parsing volume: ", err)
 			continue
 		}
-		o.Bids[price] = volume
+		ob.AddBid(price, volume)
 	}
 
 	// Copying asks
+	ob.Asks.Clear()
 	for _, ask := range snap.Asks {
 		var (
 			price  float64
@@ -91,9 +153,97 @@ func (o *Orderbook) fetch(endpoint string) (int, error) {
 			log.Println("Error parsing volume: ", err)
 			continue
 		}
-		o.Asks[price] = volume
+		ob.AddAsk(price, volume)
 	}
 	return snap.LastUpdateID, nil
+}
+
+// Applies depth event to orderbook
+func (ob *OrderBook) update(depth BinanceDepthResponse) {
+	ob.mu.Lock()
+	defer ob.mu.Unlock()
+	for _, bid := range depth.Bids {
+		price, err := strconv.ParseFloat(bid[0], 64)
+		if err != nil {
+			log.Printf("Error parsing bid price: %v", err)
+			continue
+		}
+		quantity, err := strconv.ParseFloat(bid[1], 64)
+		if err != nil {
+			log.Printf("Error parsing bid quantity: %v", err)
+			continue
+		}
+		if quantity == 0 {
+			ob.RemoveBid(price)
+		} else {
+			ob.AddBid(price, quantity)
+		}
+	}
+
+	for _, ask := range depth.Asks {
+		price, err := strconv.ParseFloat(ask[0], 64)
+		if err != nil {
+			log.Printf("Error parsing ask price: %v", err)
+			continue
+		}
+		quantity, err := strconv.ParseFloat(ask[1], 64)
+		if err != nil {
+			log.Printf("Error parsing ask quantity: %v", err)
+			continue
+		}
+		if quantity == 0 {
+			ob.RemoveAsk(price)
+		} else {
+			ob.AddAsk(price, quantity)
+		}
+	}
+}
+
+func minKey(m map[float64]float64) (minKey float64, found bool) {
+	if len(m) == 0 {
+		return 0, false
+	}
+
+	minKey = math.Inf(1) // Initialize with positive infinity
+	for key := range m {
+		if key < minKey {
+			minKey = key
+		}
+	}
+	return minKey, true
+}
+
+func manageOrderBook(ob *OrderBook, snapEndpoint string, ch <-chan BinanceDepthResponse) {
+	lastUpdateID, err := ob.fetch(snapEndpoint)
+	afterFetch := true
+	if err != nil {
+		log.Fatal(err)
+	}
+	for event := range ch {
+		if event.FinalUpdateID < lastUpdateID {
+			log.Println("Dropping event ", event.FinalUpdateID)
+			continue
+		}
+		if afterFetch && event.FirstUpdateID <= lastUpdateID && event.FinalUpdateID >= lastUpdateID {
+			log.Println("First update after fetch")
+			lastUpdateID = event.FinalUpdateID
+			ob.update(event)
+			afterFetch = false
+			continue
+		}
+		if lastUpdateID != event.PrevUpdateID {
+			log.Println("synicing...")
+			lastUpdateID, err = ob.fetch(snapEndpoint)
+			afterFetch = true
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			// Update is in correct order
+			ob.update(event)
+			lastUpdateID = event.FinalUpdateID
+		}
+	}
 }
 
 func main() {
@@ -102,20 +252,9 @@ func main() {
 	}
 	ticker := os.Args[1]
 	// Websocket binance api wsEndpoint for retrieving depth and mark price
-	wsEndpoint := fmt.Sprintf("wss://fstream.binance.com/ws/%s@depth", strings.ToLower(ticker))
+	wsEndpoint := fmt.Sprintf("wss://fstream.binance.com/ws/%s@depth@500ms", strings.ToLower(ticker))
 	// Binance API endpoint to get orderbook snapshot
-	snapEndpoint := fmt.Sprintf("https://api.binance.com/api/v3/depth?symbol=%s&limit=1000", strings.ToUpper(ticker))
-
-	ob := Orderbook{
-		Bids: make(map[float64]float64),
-		Asks: make(map[float64]float64),
-	}
-	lu, err := ob.fetch(snapEndpoint)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(ob.Bids)
-	fmt.Println(lu)
+	snapEndpoint := fmt.Sprintf("https://fapi.binance.com/fapi/v1/depth?symbol=%s&limit=1000", strings.ToUpper(ticker))
 
 	log.Printf("Connecting to %s", wsEndpoint)
 
@@ -126,18 +265,28 @@ func main() {
 	var (
 		response BinanceDepthResponse
 	)
-	wg := sync.WaitGroup{}
-	wg.Add(1)
+	// Buffer responses
+	bufferedResponses := make(chan BinanceDepthResponse, 256)
 	go func() {
-		defer conn.Close()
 		for {
 			if err := conn.ReadJSON(&response); err != nil {
-				log.Fatal("ReadJSON:", err)
+				log.Println("ReadJSON:", err)
 			}
-			fmt.Println(response.PrevUpdateID)
+			bufferedResponses <- response
 		}
-		wg.Done()
 	}()
-	wg.Wait()
+	// Fetch orderbook
+	ob := NewOrderBook()
 
+	go manageOrderBook(ob, snapEndpoint, bufferedResponses)
+
+	for {
+		ob.mu.Lock()
+		minAsk, volume, found := ob.GetBestAsk()
+		if found {
+			log.Println("Best ask: ", minAsk, " quantity: ", volume)
+		}
+		ob.mu.Unlock()
+		time.Sleep(time.Second)
+	}
 }
